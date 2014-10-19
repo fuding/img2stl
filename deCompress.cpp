003e5de7e4bdfd1a36ccf0f0c9e416a050ec041f
Dafmulder <Dafmulder@users.noreply.github.com>
2014-10-19 20:15:46 +0200

    Fix header

--- deCompress.cpp
+++ deCompress.cpp
@@ -1,9 +1,16 @@
-//============================================================================
-// Name        : deCompress.cpp
-// Author      : Joris van Hien
-// Version     : 0.1
-// Description : Used to combine an png with a stl file.
-//============================================================================
+/*
+ *     ____  ____  _____   ____________
+ *    / __ \/ __ \/  _/ | / /_  __/ __ \
+ *   / /_/ / /_/ // //  |/ / / / / /_/ /
+ *  / ____/ _, _// // /|  / / / / _, _/
+ * /_/   /_/ |_/___/_/ |_/ /_/ /_/ |_|
+ *
+ * Copyright Printr B.V. All rights reserved.
+ * This code is closed source and should under
+ * nu circumstances be copied or used in other
+ * applications that for Printr B.V.
+ *
+ */
 
 #include "deCompress.hpp"
 #include <iostream>
